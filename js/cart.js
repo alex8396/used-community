d88f89adbda28f2f1aa65a2e84c39c248409b09c
leeemingyu <skystar9927@naver.com>
2025-03-14 17:28:50 +0900

    feat: 인증 모달 구현

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,6 @@
+import { addToCart } from '../api/api';
+
 async function addToCart(prodcode) {
-    const cartResponse = await axios.post(
-        "http://localhost:8080/addToCart",
-        { prodcode }
-      );
-      const cart = cartResponse.data;
-      console.log(cart);
+    const cart = await addToCart(prodcode);
+    console.log(cart);
 }
