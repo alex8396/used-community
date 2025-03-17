21ff65069f3dfbaab124d0610af7fcb2744bc387
alex8396 <alex8396@naver.com>
2025-03-17 17:00:11 +0900

    feat: 로그인 및 회원가입 버튼 추가, 쿠키 관리 기능 구현

--- index.js
+++ index.js
@@ -145,3 +145,32 @@ window.onload = async () => {
   toggleLoginState(sessionStorage.getItem("Authorization"));
 
 };
+
+  // 유틸리티
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == " ") {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function removeCookie(cname) {
+    document.cookie = cname + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  }
+  
+    Authorization = getCookie("Authorization");
+    email = getCookie("email");
+    if (Authorization && email) {
+    //   axios.defaults.headers.common["Authorization"] = Authorization; // Authorization 헤더 설정
+      document.getElementById("loginSpan").innerHTML = `${email}  
+      <button class="btn btn-danger btn-sm" id="logoutBtn">Logout</button>`;
+    }
