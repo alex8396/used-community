7d11377bc6f62c9a15e1cee3631915f2b4774cf1
alex8396 <alex8396@naver.com>
2025-03-18 11:16:05 +0900

    feat:메인화면 상품 조회 수정

--- js/add-product.js
+++ js/add-product.js
@@ -266,7 +266,7 @@ const addProduct = () => {
                 
                 if (response.data.status === "ok") {
                     alert("상품이 등록되었습니다");
-                    window.location.reload();
+                    window.location.href = "http://192.168.219.118:5500/"
                 } else {
                     alert("상품 등록에 실패했습니다. 다시 시도해주세요");
                 }
