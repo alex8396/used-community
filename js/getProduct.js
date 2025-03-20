283c3f1d9b09cdb7a8038d6b4fa43ade885f5638
alex8396 <alex8396@naver.com>
2025-03-20 09:06:27 +0900

    feat/상세페이지구현

--- js/getProduct.js
+++ js/getProduct.js
@@ -3,15 +3,21 @@ const getProduct = (productId) => {
 
     main_data.innerHTML = `
         <div>상품 ${productId}번 페이지</div>
+        
+        
+        
     `;
 };
+
+
 
 // 페이지 로딩 시 URL에 맞게 상태 초기화
 const initGetProduct = () => {
     const path = document.location.pathname;  // 현재 URL 경로
-    const match = path.match(/\/products\/(\d+)/);  // /product/ 뒤에 숫자를 추출
-    
+    const match = path.match(/\/product\/(\d+)/);  // /product/ 뒤에 숫자를 추출
+    console.log(path, match);
     if (match) {
+        
         const productId = match[1];  // 추출한 productId
         getProduct(productId);  // 상품 데이터 가져오기
     }
